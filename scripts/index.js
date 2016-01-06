f5554ed5ab42c94055235afb93d52c99f08fc490
Matt Bierner <mattbierner@gmail.com>
2016-01-05 23:58:27 -0800

    Added toc.

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,15 @@ $(function() {
 });
 
 var SetupToc = function() {
-  $('.post-content').prepend($("<nav class='toc'></nav>"));
+  $('.entry').prepend($("<nav class='toc'></nav>"));
     
     $('.toc').toc({
-        container: '.post-content',
+        container: '.entry',
         'smoothScrolling': false
     });
     
    $(window).scroll(function() {
-       var headerH = $('.post-content').offset().top; console.log(headerH);
+       var headerH = $('.entry').offset().top; console.log(headerH);
        var scrollVal = $(this).scrollTop();
        $('.toc').css({'position': scrollVal > headerH ? 'fixed' : 'static', 'top' :'0'});
     });
