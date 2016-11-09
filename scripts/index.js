2c9a2f0607d4221fdb33175fc3f4129c0e116893
Matt Bierner <mattbierner@gmail.com>
2016-11-08 22:52:15 -0800

    Fixed toc showing up on articles with no headers

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
 "use strict";
 
+var tocSelector = '.post > h1, .post > h2';
+
 $(function () {
     SetupToc();
     $(window).scroll(OnScroll);
     OnScroll();
 });
 
+
 var SetupToc = function () {
+    if (!$(tocSelector).size())
+        return;
+    
+    $('.toc').removeClass('hidden')
     $('.toc-content').toc({
-        selectors: '.post > h1, .post > h2',
+        selectors: tocSelector,
         smoothScrolling: false,
         headerText: function (i, heading, $heading) {
             return $heading.text();
